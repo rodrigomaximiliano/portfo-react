c350ea4dfbb7a8f0b7e5a4c69ad65c9a20518992
Rodrigo Maximiliano <maxilcksly@gmail.com>
2023-12-27 11:29:30 -0300

    Correción en el footer

--- src/containers/home/footer.js
+++ src/containers/home/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__social">
-        <a href="https://www.instagram.com/rodrigomaximiliano/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/rodrigomaxmiliano/" target="_blank" rel="noopener noreferrer">
           <FaInstagram size={30} />
         </a>
         <a href="https://open.spotify.com/user/qdstxvct31et10e07pn59u0a0?si=ihOmMZpxQcK7MTNZi0UQ6w" target="_blank" rel="noopener noreferrer">
